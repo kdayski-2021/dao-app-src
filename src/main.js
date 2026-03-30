f18788c9742273c0dc137e54204ea63e6f83236d
kdayski <npoqpu6@gmail.com>
2026-03-30 13:55:21 +0500

    build fix

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
-import Vue from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
-import store from "./store";
-import vuetify from "./services/vuetify/";
-import "./sass/main.scss";
-import Bia from "@/api/bia";
+import Vue from 'vue';
+import App from './App.vue';
+import './registerServiceWorker';
+import router from './router';
+import store from './store';
+import vuetify from './services/vuetify/';
+import './sass/main.scss';
+import Bia from '@/api/bia';
 // import '@/api/index.js'
 
 Vue.config.productionTip = false;
 
 // TODO: Vuetify bug
 const ignoreWarnMessage =
-    "The .native modifier for v-on is only valid on components but it was used on <svg>.";
+    'The .native modifier for v-on is only valid on components but it was used on <svg>.';
 Vue.config.warnHandler = function(msg, vm, trace) {
     // `trace` is the component hierarchy trace
     if (msg === ignoreWarnMessage) {
@@ -30,10 +30,10 @@ new Vue({
     vuetify,
     methods: {
         async pushNotification(data) {
-            await store.dispatch("notifications/pushNotification", data);
-        }
+            await store.dispatch('notifications/pushNotification', data);
+        },
     },
-    render: h => h(App)
-}).$mount("#app");
+    render: (h) => h(App),
+}).$mount('#app');
 
 // Vue.use(VueGlobalVariable, { globals: { bia: new Bia() } });
